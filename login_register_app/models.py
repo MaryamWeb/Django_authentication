446127393bf8995f688bd7de573adee4bf055bf1
unknown <maryam_lfc@hotmail.com>
2021-02-20 22:42:15 +0300

    Create User model

--- login_register_app/models.py
+++ login_register_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import date, datetime
 
-# Create your models here.
+class User(models.Model):
+    first_name= models.CharField(max_length=40)
+    email= models.EmailField()
+    password= models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f'<User object: ID:{self.id} First Name:{self.first_name} Email:{self.email}>'
